00a6b3e5ba3692a8834b13eb3ecfd27cd7679af5
Prasant <g.prashantverma@gmail.com>
2020-12-06 23:11:20 +0530

    first commit

---
+++ 01-spring-boot-demo/src/main/java/com/java/springboot/demo/myspringbootdemo/rest/FunRestController.java
@@ -0,0 +1,17 @@
+package com.java.springboot.demo.myspringbootdemo.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+	
+	//expose "/" endpoint that return "Hello world"
+
+	@GetMapping
+	public String sayHello() {
+		return "Hello World! Time on server is "+LocalDateTime.now();
+	}
+}
